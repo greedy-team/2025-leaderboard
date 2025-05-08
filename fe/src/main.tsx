ae60dbc0fe3c8e31e2c6bf4ee5f9ca4b718dd1c3
kokodak <kokodakadokok@gmail.com>
2025-05-08 19:10:44 +0900

    Modify page scale value

--- fe/src/main.tsx
+++ fe/src/main.tsx
@@ -1,12 +1,15 @@
+// main.tsx 또는 index.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <div className="flex items-center justify-center w-screen h-screen">
-      <App />
+    <div className="w-screen h-screen flex items-center justify-center overflow-hidden">
+      <div className="scale-[0.95]">
+        <App />
+      </div>
     </div>
   </React.StrictMode>
 );
