99d9618c369c45b437672c0e73239d8d3e46f8ea
chxghee <francis001021@gmail.com>
2025-05-12 12:35:19 +0900

    feat: add cors config

---
+++ be/src/main/java/com/greedy/leaderboard/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.greedy.leaderboard.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("https://greedy-team.github.io/2025-leaderboard/")
+                .allowedMethods("GET", "POST")
+                .maxAge(3600);
+    }
+}
