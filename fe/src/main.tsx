b8fd2ca4afba0e5eb24eed40ba9cd082c844c345
kokodak <kokodakadokok@gmail.com>
2025-05-15 11:23:56 +0900

    Set auto reload feature

--- fe/src/main.tsx
+++ fe/src/main.tsx
@@ -4,6 +4,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+setInterval(() => {
+  window.location.reload();
+}, 30000);
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <div className="w-screen h-screen flex items-center justify-center overflow-hidden">
